7fe14e29a1fce8c42f89bc4134e449ee62ee4c91
Md Tahsin Sakib <tahsin15-10624@diu.edu.bd>
2022-08-31 02:29:44 +0600

    Play with JS and button event

--- scripts/script-1.js
+++ scripts/script-1.js
@@ -15,8 +15,23 @@ for(var i=0; i<SectionHeads.length; i++){
 var SectionHeads = document.querySelectorAll('.Section-head'); //.querySelectorAll
 for(var i=0; i<SectionHeads.length; i++){
     var element = SectionHeads[i];
-    if(i%2==0)
+    if((i+1)%2==0){
         element.style.backgroundColor = "blue";
-    else
+        element.setAttribute('title', 'This title is even');
+    }
+    else{
         element.style.backgroundColor = "gray"; 
+        element.setAttribute('title', 'This title is odd');
+    }
+}
+
+//Let's play with the button 'Thanks'
+var buttonClick = document.getElementById('OK');
+buttonClick.onclick=function(){
+    var putWelcomeText = document.getElementById('Welcome-txt')
+    putWelcomeText.innerHTML = "You are welcome...";
+}
+buttonClick.onmousemove =function(){
+    var putWelcomeText = document.getElementById('Welcome-txt')
+    putWelcomeText.innerHTML = "Please click me.... please!!!";
 }
