9e78f26aab4921351d3c7aa3ffe4316da56659a4
Md Tahsin Sakib <tahsin15-10624@diu.edu.bd>
2022-08-30 19:40:42 +0600

    Javascript on DOM practice

--- scripts/script-1.js
+++ scripts/script-1.js
@@ -1,1 +1,22 @@
-var firsth3 = document.getElementById('f-id');
+var firsth1 = document.getElementById('f-id');
+var randomBgColor = Math.floor(Math.random()*15695346).toString(16);
+var randomTxtColor = Math.floor(Math.random()*28625378).toString(16);
+firsth1.style.backgroundColor = "#" + randomBgColor;
+firsth1.style.color = "#" + randomTxtColor;
+
+//Select all elements using a class
+var SectionHeads = document.getElementsByClassName('Section-head');
+for(var i=0; i<SectionHeads.length; i++){
+    var element = SectionHeads[i];
+    element.innerHTML = "Head-"+(i+1);
+} 
+
+//Select all elements using a class in querry selector
+var SectionHeads = document.querySelectorAll('.Section-head'); //.querySelectorAll
+for(var i=0; i<SectionHeads.length; i++){
+    var element = SectionHeads[i];
+    if(i%2==0)
+        element.style.backgroundColor = "blue";
+    else
+        element.style.backgroundColor = "gray"; 
+}
